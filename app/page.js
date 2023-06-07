e6f4124c3e0da09603bebd4f774c7bc2e6e616ca
AnmolPlayzz <anmolisnotavailable@gmail.com>
2023-06-07 22:57:31 +0530

    localstorgae for tasks

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 'use client';
 import Image from 'next/image'
 import styles from './page.module.css'
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
-function Task({text, selected, onSelect}) {
+function Task({ text, selected, onSelect}) {
     let classname;
     if (selected) {
         classname = styles.selected
@@ -11,7 +11,7 @@ function Task({text, selected, onSelect}) {
         classname = ""
     }
     return (
-        <div onClick={} className={`${styles.task} ${classname}`}>
+        <div onClick={onSelect} className={`${styles.task} ${classname}`}>
               <i className={styles.checkbox}></i>
               <label>{text}</label>
         </div>
@@ -19,13 +19,32 @@ function Task({text, selected, onSelect}) {
 }
 
 export default function Home() {
-    const items=[
+
+    const [items,setItems]=useState([
         {id: 1, item: "item 1", done: false},
-        {id: 2, item: "item 2", done: true}]
+        {id: 2, item: "item 2", done: true}])
+    useEffect(() => {
+        const data = window.localStorage.getItem('TASKS');
+        if ( data !== null ) setItems(JSON.parse(data));
+    }, []);
+        
+    useEffect(() => {
+        window.localStorage.setItem('TASKS', JSON.stringify(items));
+    }, [items]);
+    function setItm(itm, id) {
+        const i = itm.slice()
+        if(itm[id-1].done) {
+            i[id-1].done=false
+        } else {
+            i[id-1].done=true
+        }
+        console.log(i)
+        setItems(i)
+    }
     return (
         <main className={styles.main}>
             {items.map(e => {
-                return (<Task text={e.item} selected={e.done} />)
+                return (<Task key={e.id} text={e.item} selected={e.done} onSelect={() => setItm(items,e.id)} />)
             })}
         </main>
     )
